50416d92021a216e21f30f12677b8fde249850aa
chetanv18~ <chetan@gully2global.com>
2025-04-01 10:40:14 +0530

    new

--- src/components/homesection/Vision.jsx
+++ src/components/homesection/Vision.jsx
@@ -9,22 +9,35 @@ import {
 
 const Vision = () => {
   return (
-      <div style={{ backgroundColor: '#E6DCDC' }}>
-            
-    <VisionContainer>
-      <VisionHeading>Vision</VisionHeading>
-      <VisionText>
-        Our Vision Is To Be The Premier Platform That Celebrates The Artistry Of Fashion Designers Worldwide.
-      </VisionText>
+    <div style={{ backgroundColor: '#E6DCDC' }}>
+      <VisionContainer>
+        <VisionHeading>Vision</VisionHeading>
+        <VisionText>
+          Our Vision Is To Be The Premier Platform That Celebrates The Artistry Of Fashion Designers Worldwide.
+        </VisionText>
 
-      <MissionHeading>Mission</MissionHeading>
-      <MissionText>
-        Our Mission Is To Bridge The World Of Fashion And Freedom Through A Dynamic Platform That Curates The Most Creative And Unique Apparel Designs From Talented Fashion Designers. We Are Dedicated To Revolutionizing The Fashion Industry By Treating Every Individual As A Unique Canvas For Self-Expression.
-      </MissionText>
-      <MissionText>
-        We Believe In Giving Users The Power To Choose From Our Collection Or Customize Their Clothing With Their Own Ideas, While Fostering Collaboration With Designers Who Can Bring These Visions To Life. At Sveccha, We Are Dedicated To Elevate The Essence Of Freedom And The Beauty Of Personal Expression In Fashion By Enabling Personal Style, Celebrating Diversity, And Fostering A Community Where Fashion Truly Meets Freedom.
-      </MissionText>
-    </VisionContainer>
+        <MissionHeading>Mission</MissionHeading>
+        <MissionText>
+          Indigo Rhapsody is an app-based marketplace designed for consciously crafted fashion and design. 
+          It offers a curation of works from visionary fashion designers, brands, and artisans committed 
+          to shaping the future of sustainable style.
+        </MissionText>
+        <MissionText>
+          It transforms the shopping experience into an act of purpose. Here, eco-friendly fabrics and 
+          handmade treasures weave a rhapsody of style and value, allowing customers to discover exclusive 
+          collections that elevate their wardrobe while supporting brands dedicated to ethical practices.
+        </MissionText>
+        <MissionText>
+          Indigo Rhapsody is more than just a platform. It's a movement that redefines couture by 
+          prioritizing eco-friendly accessories, handmade home décor, and fashion that empowers both the 
+          planet and its communities. Every purchase on Indigo Rhapsody is a step toward positive change, 
+          empowering buyers to join a growing movement that celebrates sustainability and conscious living.
+        </MissionText>
+        <MissionText>
+          It's easier than ever to shop with intention, knowing that each item not only looks good but 
+          also does good for the world, proving that fashion can be a force for a better tomorrow.
+        </MissionText>
+      </VisionContainer>
     </div>
   );
 };
